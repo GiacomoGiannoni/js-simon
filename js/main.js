fc901c16edb815906747886ea8021795b12b5e97
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-15 16:44:10 +0200

    parte js completata

---
+++ js/main.js
@@ -0,0 +1,55 @@
+let container = document.getElementById('container');
+let innerTime = document.getElementById('timer');
+let resultBox = document.getElementById('result');
+let randomArray = [];
+let targetArray = [];
+
+
+randomArrayGenerator(container, randomArray);
+console.log(randomArray);
+
+let counter = 0;
+let timer = setInterval(
+    function() {
+        innerTime.innerHTML = counter;
+        counter++;
+        if (counter === 31) {
+            container.classList.add('hidden');
+        } else if (counter === 32) {
+            counter = 0;
+            clearInterval(timer);
+            innerTime.innerHTML = '';
+            numberRequest();
+            const comparisonArray = randomArray.filter(number => {
+                if (targetArray.includes(number)) {
+                    return true;
+                }
+                return false;
+            });
+
+            if (comparisonArray.lenght == 0) {
+                resultBox.innerHTML = "Non sei riuscito a ricordarti nessun numero. Prova ancora!";  
+            } else {
+                resultBox.innerHTML = `I numeri che sei riuscito a ricordarti sono: <br>${comparisonArray}`;
+            }
+        }
+    }, 1000);
+
+function randomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomArrayGenerator (container, randomArray) {
+    while (randomArray.length < 5) {
+        randomArray.push(randomNumber(1, 100));
+    }
+    return container.innerHTML = `I numeri generati sono: ${randomArray}`;
+}
+
+function numberRequest () {
+    while (targetArray.length < 5) {
+        let selection = parseInt(prompt("Inserisci un numero"));
+        targetArray.push(selection);
+    }
+    console.log(targetArray);
+}
